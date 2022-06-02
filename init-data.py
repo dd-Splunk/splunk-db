45f507dcc9c2d2cd09195b95be3663c9ab281cb5
D. Dessy <ddessy@splunk.com>
2022-06-02 13:14:11 +0200

    relocate data

--- init-data.py
+++ init-data.py
@@ -1,7 +1,9 @@
 import csv
 import random
 
-filename = "./data/dc.csv"
+data_dir = "./app/lookups/"
+
+filename = data_dir + "dc.csv"
 header = ["dc_name"]
 data = []
 
@@ -19,18 +21,25 @@ with open(filename, "w", encoding="UTF8") as f:
             writer.writerow(data)
 
 # Read DCs names
-with open("./data/dc.csv") as file:
+with open(data_dir + "dc.csv") as file:
     dcs = file.read().split("\n")
 dcs.pop(0)  # Remove Header
 dcs = list(filter(None, dcs))  # remove empty strings
 
 # Read Services names
-with open("./data/services.csv") as file:
+with open(data_dir + "services.csv") as file:
     svcs = file.read().split("\n")
 svcs.pop(0)  # Remove Header
 svcs = list(filter(None, svcs))  # remove empty strings
 
-filename = "./data/dc-svc.csv"
+# Read Users
+with open(data_dir + "users.csv") as file:
+    users = file.read().split("\n")
+users.pop(0)  # Remove Header
+users = list(filter(None, users))  # remove empty strings
+
+# Generate Status
+filename = data_dir + "dc-svc.csv"
 header = ["host", "app", "status"]
 data = []
 
@@ -47,3 +56,20 @@ with open(filename, "w", encoding="UTF8") as f:
                 status = "no"
             data = [dc, svc, status]
             writer.writerow(data)
+
+# Generate mail traffic
+filename = data_dir + "send-receive.csv"
+header = ["sender", "receiver", "bytes"]
+data = []
+
+with open(filename, "w", encoding="UTF8") as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+    for i in range(200):
+        sender = random.choice(users)
+        receiver = random.choice(users)
+        bytes = random.randint(100, 1000)
+        data = [sender, receiver, bytes]
+        writer.writerow(data)
