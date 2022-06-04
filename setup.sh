94e40677de603d4acaf5c856e681cc67b02ed5ed
D. Dessy <ddessy@splunk.com>
2022-06-04 16:18:44 +0200

    combine all inits

--- setup.sh
+++ setup.sh
@@ -28,8 +28,8 @@ source .env
 CSV_DIR="./app/lookups"
 SQL="./init-sql/create-db.sql"
 # Create DB
-echo "CREATE DATABASE $MYSQL_DATABASE;
-    USE $MYSQL_DATABASE;"  > $SQL
+echo "USE $MYSQL_DATABASE;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"  > $SQL
 
 # Create Schema
 for f in `ls $CSV_DIR/*.csv`;
@@ -40,6 +40,34 @@ do
     echo "LOAD DATA INFILE '${f:1}' INTO TABLE \`$table\`
     FIELDS TERMINATED BY ','
     ENCLOSED BY '\"'
-    LINES TERMINATED BY '\\\n'
+    LINES TERMINATED BY '\\n'
     IGNORE 1 ROWS;" >> $SQL
 done
+
+# Bring up the environment
+docker compose up -d
+
+# Check Splunk availability
+until [ "$(curl -k -u admin:$SPLUNK_PASSWORD --silent --fail --connect-timeout 1 -I https://localhost:8089/servicesNS/nobody/splunk_app_db_connect/db_connect/dbxproxy/identities  )" ];
+do
+  echo --- Splunk is starting, please wait...
+  sleep 10
+done
+# Wait DBX to startup
+sleep 60
+#
+# Now that Splunk is up
+# Setup DB Connect
+#
+# Create identity
+curl -k -X POST -u admin:$SPLUNK_PASSWORD \
+https://localhost:8089/servicesNS/nobody/splunk_app_db_connect/db_connect/dbxproxy/identities \
+-d "{\"name\":\"splunk-id\",\"username\":\"$MYSQL_USER\",\"password\":\"$MYSQL_PASSWORD\"}"
+
+# Create a connection
+
+curl -k -X POST -u admin:$SPLUNK_PASSWORD \
+https://localhost:8089/servicesNS/nobody/splunk_app_db_connect/db_connect/dbxproxy/connections \
+-d "{\"name\":\"$MYSQL_DATABASE\", \"connection_type\":\"mysql\",  \
+\"host\":\"db\", \"database\":\"$MYSQL_DATABASE\", \"identity\":\"splunk-id\", \
+\"port\":\"3306\", \"timezone\":\"Europe/Brussels\"}"
