03751fc7e4ca2cefc0cf5377b8b348a4c36db00f
D. Dessy <ddessy@splunk.com>
2022-06-16 09:10:18 +0200

    Remove unnecessary diirs

--- up.sh
+++ up.sh
@@ -7,7 +7,7 @@ echo "Wait for Splunk availability"
 
 REGEX="<sessionKey>(.+)<\/sessionKey>"
 until [[ "$(curl -k -s -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/auth/login -d username=admin -d password=$SPLUNK_PASSWORD)" =~ $REGEX ]]; do
-  echo -n 's'
+  echo -n '.'
   sleep 10
 done
 # https://stackoverflow.com/questions/1891797/capturing-groups-from-a-grep-regex
